efabea485dfda49d46405c591ba67b07f01903d7
Liliyare231 <Liliyare231@mail.ru>
2023-09-08 10:53:41 +0300

    sm5

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,31 +1,16 @@
 //запрос у пользователя
-let a = prompt ('Введите стоимость товара');
-let b = prompt ('Введите количество ваших денег');
-// let otvet =document.querySelector('.otvet')
+let a = prompt('Введите число блоков');
 
 a = Number(a);
-b = Number(b);
-let text;
+let box = '<div class="box"></div>';
+let for_box = document.querySelector('.for_box');
+let x;
+for ( x = 1; x <= a; x++) {
+    let box = '<div class="box">' + x + '</div>';
+    let for_box = document.querySelector('.for_box');
+ 
 
-if(a == b){
-     text = 'Покупка прошла успешно'
-}else{
-    if(a > b){
-        let x = a-b;
-        text =('Для покупки не хватает '+ x + ' рублей')
-    }else {
-        let y = b-a;
-       text = ('Покупка совершина! Ваша сдача '+ y + ' рублей')
-    }  
+    for_box.insertAdjacentHTML('beforeend',box);
 }
 
-let box = '<div class="box">'+text+'</div>';
-let for_box = document.querySelector('.for_box');
-for_box.style.border = '1px solid red';
-for_box.style.borderRadius = '40px';
-for_box.style.fontSize = '18px';
-for_box.style.padding = '15px 30px';
-for_box.style.width = '500px';
 
-
-for_box.innerHTML = box
