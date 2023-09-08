e0320481aff915c95c5e04c4d352110d64de4c53
Liliyare231 <Liliyare231@mail.ru>
2023-09-08 09:58:58 +0300

    sm4

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,18 +1,31 @@
 //запрос у пользователя
 let a = prompt ('Введите стоимость товара');
 let b = prompt ('Введите количество ваших денег');
+// let otvet =document.querySelector('.otvet')
 
 a = Number(a);
 b = Number(b);
+let text;
 
 if(a == b){
-    console.log('Покупка прошла успешно')
+     text = 'Покупка прошла успешно'
 }else{
     if(a > b){
         let x = a-b;
-        console.log('Для покупки не хватает '+ x + ' рублей')
+        text =('Для покупки не хватает '+ x + ' рублей')
     }else {
         let y = b-a;
-        console.log('Покупка совершина! Ваша сдача '+ y + ' рублей')
+       text = ('Покупка совершина! Ваша сдача '+ y + ' рублей')
     }  
 }
+
+let box = '<div class="box">'+text+'</div>';
+let for_box = document.querySelector('.for_box');
+for_box.style.border = '1px solid red';
+for_box.style.borderRadius = '40px';
+for_box.style.fontSize = '18px';
+for_box.style.padding = '15px 30px';
+for_box.style.width = '500px';
+
+
+for_box.innerHTML = box
